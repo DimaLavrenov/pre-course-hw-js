1a7b1dfe19cfa8c17fddab90d6caabadd11a38ba
DimaLavrenov <34_skyline@mail.ru>
2024-12-14 08:00:20 +0700

    fix bag

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,4 +1,4 @@
-let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
+let rule = "sadsadsadsadasdsadasdasda";
 
 
 switch(true){
@@ -8,7 +8,7 @@ switch(true){
     case rule.length > 25:
         console.log("У меня 100 пудов все получится");
         break;
-    case rule.length = 25:
+    case rule.length == 25:
         console.log("50 на 50");
         break;
     default:
