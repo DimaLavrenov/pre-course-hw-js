5654029e20f98b9d1df9927657855983ca0850d1
DimaLavrenov <34_skyline@mail.ru>
2024-12-14 07:21:18 +0700

    fix bag

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,7 +1,4 @@
 // code
 let number = 1;
 
-console.log(number++, ++number, number--, --number); 
-console.log('yo'); 
-console.log('yo'); 
-console.log('yo');
+console.log(number++ + ',' + ++number + ',' + number-- + ',' + --number)
